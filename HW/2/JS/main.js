f6d6ca0a8451b1fcdfab6ab390e8941a56d8456f
john <johnmogi>
2019-11-18 21:22:29 +0200

    made excercise 1 2

--- HW/2/JS/main.js
+++ HW/2/JS/main.js
@@ -1,18 +1,31 @@
+/// <reference path ="jquery-3.4.1.js" />
 $(() => {
-    const userPrice = prompt("how much?")
+    const userPrice = +prompt("how much?")
 
     function addVat(userPrice) {
-        validatePrice(userPrice)
-        const vat = 17;
-        return (userPrice / 100) * (vat + 100);
-    }
+        try {
+            validatePrice(userPrice)
+            const vat = 17;
+            const total = (userPrice / 100) * (vat + 100);
+            return total
+        } catch (err) {
 
-    function validatePrice(userPrice) {
-        if (userPrice < 1) {
             alert("retry- use a positive number")
-            return false
         }
     }
-    console.group(addVat(userPrice));
+
+    function validatePrice(userPrice, errorCallback) {
+        if (userPrice < 1) {
+            errorCallback({
+                message: "Minimum must be lower than or equal to maximum"
+            });
+        }
+        // if (userPrice == isNaN) {
+        //     errorCallback({
+        //         message: "try again with a number"
+        //     });
+        // }
+    }
+    alert(addVat(userPrice))
 
 });
