c75373322f0084240bff92eedaf9d4ba84e4c1e4
johnmogi <anguru@gmail.com>
2019-11-18 13:51:05 +0200

    allmost done

--- main.js
+++ main.js
@@ -8,16 +8,52 @@ var Perfume = /** @class */ (function () {
         this.price = price;
     }
     Perfume.prototype.vat = function () {
-        console.log(this.price * 0.17);
+        var vatPrice = this.price + (this.price * 0.17);
+        console.log("this perfume ( " + this.name + " ) costs: " + vatPrice);
+        return vatPrice;
         //    let newPrice = this.price * 0.17
     };
     return Perfume;
+}());
+var Employee = /** @class */ (function () {
+    function Employee(firstName, lastName, adress, salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.salary = salary;
+    }
+    Employee.prototype.display = function () {
+        document.write("First Name" + this.firstName + "<br/>"
+            + "Last Name" + this.lastName + "<br/>"
+            + "Adress" + this.adress + "<br/>"
+            + "Salary" + this.salary + "<br/>");
+    };
+    return Employee;
 }());
 var firstCologne = new Perfume();
 firstCologne.name = "Eau De JavaScript";
 firstCologne.company = "microTransActive";
 firstCologne.amount = 30;
 firstCologne.gender = "Male";
-firstCologne.price = 199;
+firstCologne.price = 100;
 firstCologne.vat();
 // console.log(firstCologne)
+var scndCologne = new Perfume();
+scndCologne.name = "type Scripte Pour Elle";
+scndCologne.company = "Javel";
+scndCologne.amount = 99;
+scndCologne.gender = "female";
+scndCologne.price = 250;
+scndCologne.vat();
+var frstEmployee = new Employee();
+frstEmployee.firstName = "George";
+frstEmployee.lastName = "Burns";
+frstEmployee.salary = 999999;
+frstEmployee.adress = "Pardes Hanna";
+frstEmployee.display();
+var scndEmployee = new Employee();
+scndEmployee.firstName = "Moana";
+scndEmployee.lastName = "a lot";
+scndEmployee.salary = 666;
+scndEmployee.adress = "know where";
+scndEmployee.display();
--- main.ts
+++ main.ts
@@ -15,17 +15,62 @@ class Perfume  {
         this.price = price;
     }
     public vat() {
-        console.log(this.price  * 0.17)
+        let vatPrice = this.price + (this.price  * 0.17)
+        console.log("this perfume ( " + this.name + " ) costs: " + vatPrice)
+        return vatPrice
+     
     //    let newPrice = this.price * 0.17
         
     }
 }
+class Employee  {
+    public firstName: string;
+    public lastName: string;
+    public adress: string;
+    public salary: number;
 
+public constructor(firstName: string, lastName: string, adress: string, salary: number) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.adress = adress;
+    this.salary = salary;
+
+}
+public display() : void {
+document.write(
+    "First Name" + this.firstName + `<br/>`
+   + "Last Name" + this.lastName + `<br/>`
+   + "Adress" + this.adress + `<br/>`
+   + "Salary" + this.salary + `<br/>`
+)
+}
+}
 let firstCologne = new Perfume ();
 firstCologne.name = "Eau De JavaScript";
 firstCologne.company = "microTransActive";
 firstCologne.amount = 30;
 firstCologne.gender = "Male";
-firstCologne.price = 199;
+firstCologne.price = 100;
 firstCologne.vat()
 // console.log(firstCologne)
+let scndCologne = new Perfume ();
+scndCologne.name = "type Scripte Pour Elle";
+scndCologne.company = "Javel";
+scndCologne.amount = 99;
+scndCologne.gender = "female";
+scndCologne.price = 250;
+scndCologne.vat()
+
+let frstEmployee = new Employee ();
+frstEmployee.firstName = "George";
+frstEmployee.lastName = "Burns";
+frstEmployee.salary = 999999;
+frstEmployee.adress = "Pardes Hanna";
+frstEmployee.display()
+
+let scndEmployee = new Employee ();
+scndEmployee.firstName = "Moana";
+scndEmployee.lastName = "a lot";
+scndEmployee.salary = 666;
+scndEmployee.adress = "know where";
+scndEmployee.display()
