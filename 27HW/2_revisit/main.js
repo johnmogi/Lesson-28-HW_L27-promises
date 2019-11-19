9f7c3c921105305cdf93925d2b675820715b7103
john <johnmogi>
2019-11-19 12:23:35 +0200

    solved number 2 async

---
+++ 27HW/2_revisit/main.js
@@ -0,0 +1,25 @@
+function addVat() {
+    userPrice = +prompt("desired amount to add vat to:")
+    if (userPrice < 1) {
+        alert("error, desired number is too small")
+        return false
+    }
+    addVatNumber(userPrice, successCallback => successCallback, err => alert("Error: " + err.message));
+
+}
+
+function addVatNumber(numberU, successCallback, errorCallback) {
+    setTimeout(() => {
+        try {
+
+            const percent = (+numberU * 0.17)
+
+            const n = +numberU + percent
+            alert("Total is: " + n)
+            // return total
+            successCallback();
+        } catch (err) {
+            errorCallback(err);
+        }
+    }, 3000);
+}
